fb49d253463216977cb6354de33fda75da557fa4
Shawn Rahul D'Souza <shawnd29@users.noreply.github.com>
2020-07-14 22:30:27 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "\
+echo “\
 [general]\n\
-email = \"shawn.dsouza29@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
+email = \”shawn.dsouza29@gmail.com\”\n\
+“ > ~/.streamlit/credentials.toml
+echo “\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+“ > ~/.streamlit/config.toml
