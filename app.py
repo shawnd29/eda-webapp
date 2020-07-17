5bdfc30212280b7816e165ba0377c48d10d1c26b
Shawn Rahul D'Souza <shawnd29@users.noreply.github.com>
2020-07-17 11:44:43 -0400

    Updated the website and added data

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import intro
+import eda_webapp
+
+
+page = st.sidebar.selectbox("Choose a page", ["Intro", "Data Exploration","Machine Learning <Pending>"])
+if page=='Intro':
+    intro.hello()
+if page=='Data Exploration':
+    st.write('## **Streamined EDA**')
+    eda_webapp.eda_analysis()
+if page=='Machine Learning <Pending>':
+    st.header("I am waiting for an interesting package to get updated that should work for this scenario. Trust me, it will be amazing once this is perfected! :sparkles:")
---
+++ intro.py
@@ -0,0 +1,37 @@
+import streamlit as st 
+def hello():
+    st.write("# Hello! :smile:")
+    st.write("")
+    st.write("")
+    st.write("### This project was desgined to streamline the Exploratory Data Analysis (EDA) and Machine Learning commonly used in classification models")
+    st.write("")
+    st.write("These links were an inspiration for this project")
+    st.write("**The Essential Toolbox for Data Cleaning**  - https://towardsdatascience.com/the-simple-yet-practical-data-cleaning-codes-ad27c4ce0a38")
+    st.write("**Code snippets from CHRIS ALBON** - https://chrisalbon.com/")
+    st.write("**Data Cleaning with Python** - https://towardsdatascience.com/data-cleaning-in-python-the-ultimate-guide-2020-c63b88bf0a0d")
+    st.write("**EDA for Feature Selection in ML** - http://services.google.com/fh/files/misc/exploratory_data_analysis_for_feature_selection_in_machine_learning.pdf")
+    st.write("**Pythonic Data Cleaning With Pandas and NumPy** - https://realpython.com/python-data-cleaning-numpy-pandas/")
+    st.write("**Tour of Evaluation Metrics for Imbalanced Classification** - https://machinelearningmastery.com/tour-of-evaluation-metrics-for-imbalanced-classification/")
+    st.write("**A Starter Pack to EDA** - https://towardsdatascience.com/a-starter-pack-to-exploratory-data-analysis-with-python-pandas-seaborn-and-scikit-learn-a77889485baf")
+    st.write("**Building Web applications in Streamlit** - https://awesome-streamlit.readthedocs.io/en/latest/")
+    st.write("**Utilizing Machine Learning Models through Pycaret** - https://pycaret.org/classification/")
+    st.write("** Deploying Streamlit on Heroku** - https://towardsdatascience.com/deploy-streamlit-on-heroku-9c87798d2088")
+
+    st.write("")
+    st.write("")
+    st.write("To get started, head over through the menu on the left.")
+    st.write("")
+    st.write("")
+
+
+    st.write("For links to the Github Code, do visit:")
+    st.write("To recommend any suggestions, do reach out to me thorugh LinkenIn ")
+
+
+if __name__ == "__main__":
+    hello()
+
+
+
+
+
